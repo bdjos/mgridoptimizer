1ef89c93633ae922fb190584faba2987c8ca127d
bdjos <42777251+bdjos@users.noreply.github.com>
2018-12-06 13:18:55 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+   name='mgridoptimizer',
+   version='1.0',
+   description='Model and optimize a microgrid/grid connected DER system',
+   author='Brian Joseph',
+   author_email='briandjoseph@gmail.com',
+   packages=['mgridoptimizer'],  #same as name
+   install_requires=['pandas', 'matplotlib'], #external packages as dependencies
+)
