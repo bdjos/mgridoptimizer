8fce545049e1bf6aff032bfa364119f235d09a00
bdjos <42777251+bdjos@users.noreply.github.com>
2018-12-06 13:44:26 -0500

    Added data to package folders

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
    description='Model and optimize a microgrid/grid connected DER system',
    author='Brian Joseph',
    author_email='briandjoseph@gmail.com',
-   packages=['mgridoptimizer', 'mgridoptimizer.modules'],
+   packages=['mgridoptimizer', 'mgridoptimizer.modules', 'data',],
    install_requires=['pandas', 'matplotlib'], #external packages as dependencies
 )
