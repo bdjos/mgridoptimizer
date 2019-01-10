cccae0a4feccc52edf55f3144d017f565a6834e8
bjoseph <briandjoseph@gmail.com>
2019-01-10 13:34:25 -0500

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
    name='mgridoptimizer',
-   version='1.0.3',
+   version='1.0.4',
    description='Model and optimize a microgrid/grid connected DER system',
    author='Brian Joseph',
    author_email='briandjoseph@gmail.com',
